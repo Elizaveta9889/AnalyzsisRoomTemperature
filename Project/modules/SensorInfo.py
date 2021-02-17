3dcd036b256ea5cffd314bdfdfd38bd2b91338f7
Elizaveta9889 <45589684+Elizaveta9889@users.noreply.github.com>
2021-02-17 20:25:10 +0500

    Updated get_sensor_info
    Add new parameter as sep to get_sensor_info method

--- Project/modules/SensorInfo.py
+++ Project/modules/SensorInfo.py
@@ -7,19 +7,19 @@ def get_sensor_info_from_url(url, sep=" "):
 
 
 # get all sensor data
-def get_sensor_info(sensor_info, first_date_time=None):
+def get_sensor_info(sensor_info, sep=" ", first_date_time=None):
     # data_count needs to check that all data was added
     data_count = 0
     # get dataframes from the URL
     for sensor in sensor_info.keys():
-        sensor_info[sensor] = get_sensor_info_from_url(sensor_info[sensor])
+        sensor_info[sensor] = get_sensor_info_from_url(sensor_info[sensor], sep)
         data_count += sensor_info[sensor].shape[0]
         # rename temp column for every sensor
         sensor_info[sensor] = sensor_info[sensor].rename(columns={"temp": sensor + "_temp"})
     # create summary df
     sensor_info_val = sensor_info.values()
     # convert values of time column to time
-    df = pd.concat(sensor_info_val)
+    df = pd.concat(sensor_info_val, sort=True)
     dc.df_add_datetime(df)
     # delete loaded data
     if first_date_time is not None:
