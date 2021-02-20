8ded1446968d6cc87bd3e062df62f87d12857016
Elizaveta9889 <45589684+Elizaveta9889@users.noreply.github.com>
2021-02-20 22:11:57 +0500

    Added SensorWeatherInfo.py
    SensorInfo module (merge sensor and weather info):
    merged_dataframes
    get_dataset
    update_dataset

---
+++ Project/modules/SensorWeatherInfo.py
@@ -0,0 +1,26 @@
+from modules import SensorInfo as sinf
+from modules import WeatherInfo as winf
+
+
+def merged_dataframes(sensor_info, weather_info):
+    merged = sensor_info.merge(weather_info, left_on="date_time", right_on="date_time", how="left")
+    merged.drop_duplicates(keep="first", inplace=True)
+    return merged
+
+
+# if we don't have dataset
+def get_dataset(sensor_dict):
+    # get all sensor info
+    new_sensor_info = sinf.get_sensor_info(sensor_dict)
+    # get period of data (need to get weather info)
+    new_weather_info = winf.get_weather_info_period(new_sensor_info.index[0], new_sensor_info.tail(1).index[0])
+    return merged_dataframes(new_sensor_info, new_weather_info)
+
+
+# if we already have dataset, we need to update it to now
+def update_dataset(sensor_dict, origin_df):
+    # get new sensor info since last uploaded time
+    new_sensor_info = sinf.get_sensor_info(sensor_dict, origin_df.tail(1).index[0])
+    # get period of data (need to get weather info)
+    new_weather_info = winf.get_weather_info_period(new_sensor_info.index[0], new_sensor_info.tail(1).index[0])
+    return origin_df.append(merged_dataframes(new_sensor_info, new_weather_info))
